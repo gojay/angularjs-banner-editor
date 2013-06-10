8c4b760f2c4d34b9f08b3a842429694136fc9dba
Dani Gojay <dani.gojay@gmail.com>
2013-06-10 17:07:16 +0700

    fix minor : title, ui page creator

--- upload.php
+++ upload.php
@@ -20,30 +20,39 @@ $fileName = $_FILES['file']['name'];
 $fileType = $_FILES['file']['type'];
 $ext      = pathinfo($fileName, PATHINFO_EXTENSION);
 
-// do upload n resize
-$imagehand = new upload( $fileImg );
-$imagehand->file_dst_name_ext  = $ext;
-$imagehand->file_new_name_body = $name;
-$imagehand->file_overwrite     = true;
-$imagehand->image_resize       = true;
-if( $isCrop ) {
-	$imagehand->image_ratio_crop = true;
-	$imagehand->image_ratio_fill = true;	
+try{
+	// do upload n resize
+	$imagehand = new upload( $fileImg );
+	$imagehand->file_dst_name_ext  = $ext;
+	$imagehand->file_new_name_body = $name;
+	$imagehand->file_overwrite     = true;
+	$imagehand->image_resize       = true;
+	if( $isCrop ) {
+		$imagehand->image_ratio_crop = true;
+		$imagehand->image_ratio_fill = true;	
+	}
+	$imagehand->image_x            = $width;
+	$imagehand->image_y            = $height;
+	$imagehand->image_convert      = $ext;
+	$imagehand->Process($upload_dir);
+	$imagehand->processed;
+
+	sleep(3);
+
+	$imgURL     = 'http://dev.angularjs/_learn_/angularjs-banner-editor/uploads/' . $imagehand->file_dst_name;
+	$imgDataUri = 'data:' . $fileType . ';base64,' . base64_encode(file_get_contents($imagehand->file_dst_pathname));
+
+	// send response
+	echo json_encode(array(
+		'url'     => $imgURL,
+		'dataURI' => $imgDataUri
+	));
+} 
+catch(Exception $e) {
+	echo json_encode(array(
+		'status'  => false,
+		'message' => $e->getMessage()
+	));
 }
-$imagehand->image_x            = $width;
-$imagehand->image_y            = $height;
-$imagehand->image_convert      = $ext;
-$imagehand->Process($upload_dir);
-if( !$imagehand->processed ) throw new Exception("Error upload $fileName");
 
-sleep(3);
-
-$imgURL     = 'http://dev.angularjs/_learn_/angularjs-banner-editor/uploads/' . $imagehand->file_dst_name;
-$imgDataUri = 'data:' . $fileType . ';base64,' . base64_encode(file_get_contents($imagehand->file_dst_pathname));
-
-// send response
-echo json_encode(array(
-	'url'     => $imgURL,
-	'dataURI' => $imgDataUri
-));
 ?>
